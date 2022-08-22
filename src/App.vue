1e540f6c136e971ac586374196510526dcad5e12
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2022-08-22 10:13:38 +0900

    20220821_最初のcommitです。

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>都道府県一覧</h2>
+  </div>
+</template>
+
+<script>
+const apiUrl = "https://opendata.resas-portal.go.jp/api/v1/prefectures"
+const apiKey = "nyB4LKskiLFNzAYzdoIcbnaovQkWIvATq6O9GOCc"
+const config = {headers: {
+    'Content-Type': 'application/json',
+    'x-api-key': apiKey
+}}
+
+import axios from "axios";
+export default {
+  data() {
+    return {
+      result:[]
+    }
+  },
+  created() {
+    axios.get(apiUrl, config)
+      .then(response => {
+        this.result = response.data.result;
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
