cdb64a8f2b3a031a8ec15892c88e531a029b72a3
amahaya0703 <61222005+amahaya0703@users.noreply.github.com>
2022-08-22 23:13:59 +0900

    20220822_二日目のcommitです。

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
   <div>
     <h2>都道府県一覧</h2>
+    <Prefectures :prefPosts="posts"></Prefectures>
   </div>
 </template>
 
 <script>
 const apiUrl = "https://opendata.resas-portal.go.jp/api/v1/prefectures"
 const apiKey = "nyB4LKskiLFNzAYzdoIcbnaovQkWIvATq6O9GOCc"
-const config = {headers: {
+const config = {
+  headers: {
     'Content-Type': 'application/json',
     'x-api-key': apiKey
-}}
-
+  }
+};
+import Prefectures from "./components/Prefectures.vue";
 import axios from "axios";
+
+
+
 export default {
   data() {
     return {
-      result:[]
+      posts: []
     }
+  },
+  components: {
+    Prefectures
   },
   created() {
     axios.get(apiUrl, config)
       .then(response => {
-        this.result = response.data.result;
+        this.posts = response.data.result;
     })
   }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/Prefectures.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="app">
+    <ul>
+      <li v-for="prefPost in prefPosts" :key="prefPost.prefName">
+      <label for="pref">
+        <input
+        type="checkbox"
+        id="pref"
+        v-model="eventData.target"
+        >
+        {{prefPost.prefName}}
+      </label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["prefPosts"],
+  data() {
+    return {
+      eventData: {
+        target: []
+      }
+    }
+  }
+
+
+}
+</script>
+
+
+<style scoped>
+
+.app{
+  margin: 0 auto;
+  width: 250px;
+}
+
+ul{
+  padding-left: 0px;
+  display: grid;
+  grid-template-columns: 3fr 3fr 3fr 3fr;
+}
+
+li{
+  list-style: none;
+  width: 100px;
+  margin: 0 auto;
+}
+</style>
